b70cc4df14d0aaa4ac5b8a5cb8bf3717a8515895
max1220 <max1220@users.noreply.github.com>
2017-04-03 19:58:11 +0000

    Input support, translation of names via keysymdef possible

--- Makefile
+++ Makefile
@@ -4,10 +4,24 @@
 CFLAGS= -O3 -Wall -Wextra -fPIC -I/usr/include/lua5.1 -I../lfb -Ilibvncserver -Ilibvncserver/libvncserver
 LIBS= -shared -llua5.1 -lvncserver
 
+.PHONY: clean all
+.DEFAULT_GOAL := all
+
+all: vnc.so keysymdef.lua
+
 vnc.so: vnc.c
 	$(CC) -o $@ $(CFLAGS) $(LIBS) $<
 	strip $@
 
+keysymdef.h:
+	wget "https://cgit.freedesktop.org/xorg/proto/x11proto/plain/keysymdef.h"
+
+keysymdef.lua: keysymdef.h
+	./defines_to_table.lua keysymdef.h > keysymdef.lua
+
 
 clean:
 	rm -f vnc.so
+
+
+# 
--- vnc.c
+++ vnc.c
@@ -26,6 +26,10 @@
 #define SAMPLES_PER_PIXEL 3
 #define BYTES_PER_PIXEL 4
 
+#define MAX_KEY_EVENT_COUNT 1024
+#define MAX_MOUSE_EVENT_COUNT 1024
+
+#define LUA_T_PUSH_S_B(S, N) lua_pushstring(L, S); lua_pushboolean(L, N); lua_settable(L, -3);
 #define LUA_T_PUSH_S_N(S, N) lua_pushstring(L, S); lua_pushnumber(L, N); lua_settable(L, -3);
 #define LUA_T_PUSH_S_S(S, S2) lua_pushstring(L, S); lua_pushstring(L, S2); lua_settable(L, -3);
 #define LUA_T_PUSH_S_CF(S, CF) lua_pushstring(L, S); lua_pushcfunction(L, CF); lua_settable(L, -3);
@@ -36,6 +40,24 @@ typedef struct {
     rfbScreenInfoPtr screen;
 } server_t;
 
+typedef struct {
+	rfbBool down;
+	rfbKeySym key;
+	rfbClientPtr client;
+} key_event_t;
+
+typedef struct {
+	int button_mask;
+	int x;
+	int y;
+} mouse_event_t;
+
+
+key_event_t key_event_stack[MAX_KEY_EVENT_COUNT];
+int key_event_stack_index = 0;
+
+mouse_event_t mouse_event_stack[MAX_MOUSE_EVENT_COUNT];
+int mouse_event_stack_index = 0;
 
 
 static int vnc_server_is_active(lua_State *L) {
@@ -100,8 +122,6 @@ static int vnc_server_draw_from_drawbuffer(lua_State *L) {
     
     pixel_t p;
 
-	//printf("%d %d   %d %d   %d %d\n", target_x, target_y,  origin_x, origin_y,  w, h);
-
     for (cy=0; cy < h; cy=cy+1) {
         for (cx=0; cx < w; cx=cx+1) {
 			tx = target_x+cx;
@@ -113,21 +133,21 @@ static int vnc_server_draw_from_drawbuffer(lua_State *L) {
             	oy < 0 || oy >= db->h-1 || \
             	tx < 0 || tx >= server->w-1 ||
             	ty < 0 || ty >= server->h-1) {
-					
-				// printf("\nOut of bounds!\n\n");
-					
             	continue;
             } else {
 				p = db->data[oy*db->w+ox];
-            
-				// printf("\nx: %d, y: %d, r,g,b: %d,%d,%d\n\n", cx,cy,p.r,p.g,p.b);
-            
-				screen->frameBuffer[(ty*server->w+tx)*BYTES_PER_PIXEL+0] = p.r;
-				screen->frameBuffer[(ty*server->w+tx)*BYTES_PER_PIXEL+1] = p.g;
-				screen->frameBuffer[(ty*server->w+tx)*BYTES_PER_PIXEL+2] = p.b;
+				if (p.a > 0) {
+					screen->frameBuffer[(ty*server->w+tx)*BYTES_PER_PIXEL+0] = p.r;
+					screen->frameBuffer[(ty*server->w+tx)*BYTES_PER_PIXEL+1] = p.g;
+					screen->frameBuffer[(ty*server->w+tx)*BYTES_PER_PIXEL+2] = p.b;
+				}
 			}            
         }
     }
+    
+    if (lua_toboolean(L, 9)) {
+		rfbMarkRectAsModified(server->screen,target_x,target_y,w,h);
+	}
 
     return 0;
 }
@@ -146,22 +166,42 @@ static int vnc_server_update_rect(lua_State *L) {
     return 0;
 }
 
-static int vnc_server_set_on_key(lua_State *L) {
-    server_t *server = (server_t *)lua_touserdata(L, 1);
-    rfbScreenInfoPtr screen = server->screen;
-    
-    // TODO
-    
-    return 0;
+
+static int vnc_server_read_key(lua_State *L) {
+	key_event_t ev;
+	
+	if (key_event_stack_index > 0) {
+		ev = key_event_stack[key_event_stack_index];
+	
+	    lua_newtable(L);
+		LUA_T_PUSH_S_N("down", (int8_t)ev.down);
+		LUA_T_PUSH_S_N("key", (uint32_t)ev.key);
+		
+		key_event_stack_index = key_event_stack_index - 1;
+			
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
-static int vnc_server_set_on_mouse(lua_State *L) {
-    server_t *server = (server_t *)lua_touserdata(L, 1);
-    rfbScreenInfoPtr screen = server->screen;
-    
-    // TODO
-    
-    return 0;
+static int vnc_server_read_mouse(lua_State *L) {
+	mouse_event_t ev;
+	
+	if (mouse_event_stack_index > 0) {
+		ev = mouse_event_stack[mouse_event_stack_index];
+		
+	    lua_newtable(L);
+		LUA_T_PUSH_S_N("button_mask", ev.button_mask);
+		LUA_T_PUSH_S_N("x", ev.x);
+		LUA_T_PUSH_S_N("y", ev.y);
+			
+		mouse_event_stack_index = mouse_event_stack_index - 1;
+		
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 static int vnc_server_tostring(lua_State *L) {
@@ -170,6 +210,45 @@ static int vnc_server_tostring(lua_State *L) {
     lua_pushfstring(L, "vncserver: %dx%d", server->w, server->h);
     
     return 0;
+}
+
+static void handle_key(rfbBool down, rfbKeySym key, rfbClientPtr cl) {
+	int k;
+	if (key_event_stack_index-1 == MAX_KEY_EVENT_COUNT) {
+		for (k = key_event_stack_index; k > MAX_KEY_EVENT_COUNT; k--){   
+			key_event_stack[k] = key_event_stack[k-1];
+		}
+	} else {
+		key_event_stack_index = key_event_stack_index + 1;
+	}
+	
+	key_event_t ev = {
+		.down = down,
+		.key = key,
+		.client = cl
+	};
+	key_event_stack[key_event_stack_index] = ev;
+}
+
+static void handle_mouse(int button_mask, int x, int y,rfbClientPtr cl) {
+	
+	int k;
+	if (mouse_event_stack_index-1 == MAX_MOUSE_EVENT_COUNT) {
+		for (k = mouse_event_stack_index; k > MAX_MOUSE_EVENT_COUNT; k--){   
+			mouse_event_stack[k] = mouse_event_stack[k-1];
+		}
+	} else {
+		mouse_event_stack_index = mouse_event_stack_index + 1;
+	}
+	
+	mouse_event_t ev = {
+		.button_mask = button_mask,
+		.x = x,
+		.y = y
+	};
+	mouse_event_stack[mouse_event_stack_index] = ev;
+	
+	rfbDefaultPtrAddEvent(button_mask,x,y,cl);
 }
 
 static int l_new_server(lua_State *L) {
@@ -181,6 +260,9 @@ static int l_new_server(lua_State *L) {
 	server->screen = screen;
 	
 	screen->autoPort = TRUE;
+	
+	screen->kbdAddEvent = handle_key;
+	screen->ptrAddEvent = handle_mouse;
 	
 	screen->frameBuffer = calloc(server->w * server->h, BYTES_PER_PIXEL);    
     
@@ -197,9 +279,10 @@ static int l_new_server(lua_State *L) {
     LUA_T_PUSH_S_CF("set_pixel", vnc_server_set_pixel)
     LUA_T_PUSH_S_CF("draw_from_drawbuffer", vnc_server_draw_from_drawbuffer)
     LUA_T_PUSH_S_CF("update_rect", vnc_server_update_rect)
-    LUA_T_PUSH_S_CF("vnc_server_set_on_key", vnc_server_set_on_key)
-    LUA_T_PUSH_S_CF("vnc_server_set_on_mouse", vnc_server_set_on_mouse)
+    LUA_T_PUSH_S_CF("read_key", vnc_server_read_key)
+    LUA_T_PUSH_S_CF("read_mouse", vnc_server_read_mouse)
     LUA_T_PUSH_S_CF("__tostring", vnc_server_tostring)
+
     lua_setmetatable(L, -2);
     
     return 1;
